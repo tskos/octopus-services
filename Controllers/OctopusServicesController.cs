cec38756bd4a7bb4cc7240b4fa4dd9512b2d0b07
Constantine Tsvigun <kostyantyn.tsvigun@gmail.com>
2018-05-11 15:28:11 +0300

    Controller route configured

---
+++ Controllers/OctopusServicesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OctopusServices.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class OctopusServicesController : Controller
+    {
+        [HttpGet]
+        public ActionResult CountWordLengths()
+        {
+            return new JsonResult("test");
+        }
+    }
+}
